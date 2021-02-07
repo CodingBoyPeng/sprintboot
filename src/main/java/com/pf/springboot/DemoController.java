4a2f89fdb9fd80b32737efd9e6f94ce098520f1d
pengfeng <feng.peng@adinall.com>
2021-02-07 16:41:51 +0800

    Jenkins测试maven

--- src/main/java/com/pf/springboot/DemoController.java
+++ src/main/java/com/pf/springboot/DemoController.java
@@ -1,17 +1,17 @@
-package com.pf.springboot;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @description:
- * @author:Peng
- * @Date:2021/2/7
- */
-@RestController
-public class DemoController {
-    @GetMapping
-    public String demo(){
-        return "Hello World";
-    }
-}
+//package com.pf.springboot;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @description:
+// * @author:Peng
+// * @Date:2021/2/7
+// */
+//@RestController
+//public class DemoController {
+//    @GetMapping
+//    public String demo(){
+//        return "Hello World";
+//    }
+//}
